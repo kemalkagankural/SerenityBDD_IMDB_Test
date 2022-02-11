22369115ced99ca0b94aa20a5d128d2ebb4c131f
kemalkagankural <kemalkagankural@gmail.com>
2022-02-11 15:57:13 +0300

    Add new Scenario

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features",
-        tags="@imdb_search"
+        tags="@imdb_films"
 )
 public class CucumberTestSuite {}
